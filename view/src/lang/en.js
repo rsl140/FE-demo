c922fdf86aa25baba35b4a955015a23e923ca3bc
rsl140 <rsl140@qq.com>
2019-02-12 16:28:40 +0800

    first add

---
+++ view/src/lang/en.js
@@ -0,0 +1,101 @@
+export default {
+  route: {
+    Dashboard: 'Dashboard',
+    Setting: 'Setting',
+    Message: 'Message',
+    MessageDetail: 'MessageDetail',
+    i18n: 'i18n',
+    Example: 'Example',
+    Dragrid: 'Dragrid',
+    School: 'school',
+    Permission: 'permission',
+    Manager: 'Manager',
+    Org: 'Org',
+    // Teacher: 'Teacher',
+    // Student: 'Student',
+    // Aide: 'Aide',
+    Person: 'Person',
+    Room: 'Room',
+    Period: 'Period',
+    Subject: 'Subject',
+    organization: 'organization',
+    Term: 'Term',
+    Course: 'Course',
+    Elective: 'Elective',
+    Lesson: 'Lesson',
+    PreparingLessons: 'preparingLessons',
+    Answer: 'answer',
+    Review: 'review',
+    CourseIndex: 'CourseStudent',
+    TeacherTeam: 'TeacherTeam',
+    ClassStudent: 'ClassStudent',
+    ExpandingData: 'ExpandingData',
+    CrouseIntroduction: 'CrouseSetting',
+    ArticleDetail: 'ArticleDetail'
+  },
+  navbar: {
+    logOut: 'logOut',
+    dashboard: 'dashboard',
+    github: 'github',
+    screenfull: 'screenfull',
+    theme: 'theme'
+  },
+  login: {
+    title: 'Sys login',
+    logIn: 'logIn',
+    username: 'username',
+    password: 'password',
+    any: 'any',
+    thirdparty: 'thirdparty'
+  },
+  permission: {
+    roles: 'roles',
+    switchRoles: 'switchRoles'
+  },
+  table: {
+    dynamicTips1: '固定表头, 按照表头顺序排序',
+    dynamicTips2: '不固定表头, 按照点击顺序排序',
+    dragTips1: '默认顺序',
+    dragTips2: '拖拽后顺序',
+    title: '标题',
+    importance: '重要性',
+    type: '类型',
+    remark: '点评',
+    search: '搜索',
+    add: '添加',
+    export: '导出',
+    reviewer: '审核人',
+    id: '序号',
+    date: '时间',
+    author: '作者',
+    readings: '阅读数',
+    status: '状态',
+    actions: '操作',
+    edit: '编辑',
+    publish: '发布',
+    draft: '草稿',
+    delete: '删除',
+    cancel: '取 消',
+    confirm: '确 定'
+  },
+  excel: {
+    export: '导出',
+    selectedExport: '导出已选择项',
+    placeholder: '请输入文件名(默认excel-list)'
+  },
+  zip: {
+    export: '导出',
+    placeholder: '请输入文件名(默认file)'
+  },
+  theme: {
+    change: '换肤',
+    documentation: '换肤文档',
+    tips:
+      'Tips: 它区别于 navbar 上的 theme-pick, 是两种不同的换肤方法，各自有不同的应用场景，具体请参考文档。'
+  },
+  tagsView: {
+    close: '关闭',
+    closeOthers: '关闭其它',
+    closeAll: '关闭所有'
+  }
+}
---
+++ view/src/lang/zh.js
@@ -0,0 +1,46 @@
+export default {
+  route: {
+    Dashboard: '首页',
+    Form: '日报'
+  },
+  navbar: {
+    logOut: '退出登录',
+    dashboard: '首页',
+    github: '项目地址',
+    screenfull: '全屏',
+    theme: '换肤',
+    home: '主页'
+  },
+  login: {
+    title: '系统登录',
+    logIn: '登录',
+    username: '账号',
+    password: '密码',
+    any: '随便填',
+    thirdparty: '第三方登录'
+  },
+  permission: {
+    roles: '你的权限',
+    switchRoles: '切换权限'
+  },
+  excel: {
+    export: '导出',
+    selectedExport: '导出已选择项',
+    placeholder: '请输入文件名(默认excel-list)'
+  },
+  zip: {
+    export: '导出',
+    placeholder: '请输入文件名(默认file)'
+  },
+  theme: {
+    change: '换肤',
+    documentation: '换肤文档',
+    tips:
+      'Tips: 它区别于 navbar 上的 theme-pick, 是两种不同的换肤方法，各自有不同的应用场景，具体请参考文档。'
+  },
+  tagsView: {
+    close: '关闭',
+    closeOthers: '关闭其它',
+    closeAll: '关闭所有'
+  }
+}
